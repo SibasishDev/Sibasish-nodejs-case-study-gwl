d47d61bccd58fd151ea1a2b703f5844099d73dbf
Sibasish Das <dassibasish46@gmail.com>
2024-05-01 20:05:48 +0530

    add date validation

--- src/controller/task.controller.js
+++ src/controller/task.controller.js
@@ -1,5 +1,6 @@
 const taskValidation = require("./task.validation");
 const { dbOpreation } = require("../modal/task.modal");
+const moment = require("moment");
 class TaskController {
   async createTask(req, res) {
     try {
@@ -12,8 +13,22 @@ class TaskController {
 
       const { title, task_dec, dueDate } = value;
 
+      console.log(moment(dueDate).format(
+        "YYYY-MM-DD"
+      ));
+
+      const currentDate = new Date();
+
+      const inputDueDate = new Date(dueDate);
+
+      if (inputDueDate < currentDate)
+        return res.status(400).json({
+          code: 400,
+          message: "Due date should be greater than current date!",
+        });
+
       const query = `INSERT INTO tasks (title, task_desc, due_date) 
-        VALUES ('${title}', '${task_dec}', '${dueDate}')`;
+        VALUES ('${title}', '${task_dec}', '${moment(dueDate).format("YYYY-MM-DD")}')`;
 
       const result = await dbOpreation(query);
 
@@ -37,166 +52,176 @@ class TaskController {
     }
   }
 
-  async getTaskById(req,res){
-    try{
-        const {taskId} = req.params;
+  async getTaskById(req, res) {
+    try {
+      const { taskId } = req.params;
 
-        if(!taskId) return res.status(400).json({
-            code : 400,
-            message : "Task id is required!"
-        })
-
-        const query = `SELECT * FROM tasks WHERE id = ${taskId}`;
-
-        const result = await dbOpreation(query);
-
-        console.log(result);
-
-        if(!result.length) return res.status(200).json({
-            code : 404,
-            message : "Task not found!"
-        })
-
-        return res.status(200).json({
-            code : 200,
-            message : "Task found successfully!",
-            data : result
-        })
-    }catch(e){
-        console.log(e);
+      if (!taskId)
         return res.status(400).json({
-            code : 400,
-            message : e.message
-        })
+          code: 400,
+          message: "Task id is required!",
+        });
+
+      const query = `SELECT * FROM tasks WHERE id = ${taskId}`;
+
+      const result = await dbOpreation(query);
+
+      console.log(result);
+
+      if (!result.length)
+        return res.status(200).json({
+          code: 404,
+          message: "Task not found!",
+        });
+
+      return res.status(200).json({
+        code: 200,
+        message: "Task found successfully!",
+        data: result,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        code: 400,
+        message: e.message,
+      });
     }
   }
 
-  async updateTask(req,res){
-      try{
-        const {taskId} = req.params;
+  async updateTask(req, res) {
+    try {
+      const { taskId } = req.params;
 
-        if(!taskId) return res.status(400).json({
-            code : 400,
-            message : "Task id is required!"
-        });
-
-        let query = `SELECT * FROM tasks WHERE id = ${taskId}`;
-
-        const result = await dbOpreation(query);
-
-        if(!result.length) return res.status(200).json({
-            code : 404,
-            message : "Task not found!"
-        });
-
-        const {error, value} = taskValidation.updateTaskValidation(req.body);
-
-        if(error) return res.status(400).json({code : 400, message : error.details[0].message});
-
-        const {title, task_desc, dueDate} = value;
-
-         query = `UPDATE tasks SET `;
-
-         if(title) query += `title = '${title}', `;
-
-         if(task_desc) query += `task_desc = '${task_desc}', `;
-
-         if(dueDate){
-            const currentDate = new Date();
-
-            const inputDueDate = new Date(dueDate);
-
-            if(inputDueDate < currentDate) return res.status(400).json({ 
-                code : 400,
-                message : "Due date should be greater than current date!"
-            });
-            query += `due_date = '${dueDate}', `;
-         } 
-
-         query = query.slice(0, -2);
-
-         query += `WHERE id = ${taskId}`;
-
-         const updateResult = await dbOpreation(query);
-
-         if(updateResult.affectedRows == 0) return res.status(200).json({
-            code : 200,
-            message : "Task not updated!"
-        });
-
-        return res.status(200).json({
-            code : 200,
-            message : "Task updated successfully!"
-        })
-
-      }catch(e){
-        console.log(e);
+      if (!taskId)
         return res.status(400).json({
-            code : 400,
-            message : e.message
-        })
-      }
-  }
+          code: 400,
+          message: "Task id is required!",
+        });
 
-  async deleteTask(req,res){
-      try{
-          const {taskId} = req.params;
+      let query = `SELECT * FROM tasks WHERE id = ${taskId}`;
 
-          if(!taskId) return res.status(400).json({
-              code : 400,
-              message : "Task id is required!"
+      const result = await dbOpreation(query);
+
+      if (!result.length)
+        return res.status(200).json({
+          code: 404,
+          message: "Task not found!",
+        });
+
+      const { error, value } = taskValidation.updateTaskValidation(req.body);
+
+      if (error)
+        return res
+          .status(400)
+          .json({ code: 400, message: error.details[0].message });
+
+      const { title, task_desc, dueDate } = value;
+
+      query = `UPDATE tasks SET `;
+
+      if (title) query += `title = '${title}', `;
+
+      if (task_desc) query += `task_desc = '${task_desc}', `;
+
+      if (dueDate) {
+        const currentDate = new Date();
+
+        const inputDueDate = new Date(dueDate);
+
+        if (inputDueDate < currentDate)
+          return res.status(400).json({
+            code: 400,
+            message: "Due date should be greater than current date!",
           });
+        query += `due_date = '${moment(dueDate).format(
+            "YYYY-MM-DD"
+          )}', `;
+      }
 
-        let query = `SELECT * FROM tasks WHERE id = ${taskId}`;
+      query = query.slice(0, -2);
 
-        const result = await dbOpreation(query);
+      query += `WHERE id = ${taskId}`;
 
-        if(!result.length) return res.status(200).json({
-            code : 404,
-            message : "Task not found!"
+      const updateResult = await dbOpreation(query);
+
+      if (updateResult.affectedRows == 0)
+        return res.status(200).json({
+          code: 200,
+          message: "Task not updated!",
         });
 
-        await dbOpreation(`DELETE FROM tasks WHERE id = ${taskId}`);
-
-        return res.status(200).json({
-            code : 200,
-            message : "Task deleted successfully!"
-        })
-
-      }catch(e){
-        console.log(e);
-        return res.status(400).json({
-            code : 400,
-            message : e.message
-        })
-      }
+      return res.status(200).json({
+        code: 200,
+        message: "Task updated successfully!",
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        code: 400,
+        message: e.message,
+      });
+    }
   }
 
-  async getAllTasks(req,res){
-      try{
+  async deleteTask(req, res) {
+    try {
+      const { taskId } = req.params;
 
-        const query = `SELECT * FROM tasks order by id desc`;
-
-        const result = await dbOpreation(query);
-
-        if(!result.length) return res.status(200).json({
-            code : 404,
-            message : "No task found!"
+      if (!taskId)
+        return res.status(400).json({
+          code: 400,
+          message: "Task id is required!",
         });
 
-        return res.status(200).json({
-            code : 200,
-            message : "Task found successfully!",
-            data : result
-        })
+      let query = `SELECT * FROM tasks WHERE id = ${taskId}`;
 
-      }catch(e){
-        console.log(e);
-        return res.status(400).json({
-            code : 400,
-            message : e.message
-        })
-      }
+      const result = await dbOpreation(query);
+
+      if (!result.length)
+        return res.status(200).json({
+          code: 404,
+          message: "Task not found!",
+        });
+
+      await dbOpreation(`DELETE FROM tasks WHERE id = ${taskId}`);
+
+      return res.status(200).json({
+        code: 200,
+        message: "Task deleted successfully!",
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        code: 400,
+        message: e.message,
+      });
+    }
+  }
+
+  async getAllTasks(req, res) {
+    try {
+      const query = `SELECT * FROM tasks order by id desc`;
+
+      const result = await dbOpreation(query);
+
+      if (!result.length)
+        return res.status(200).json({
+          code: 404,
+          message: "No task found!",
+        });
+
+      return res.status(200).json({
+        code: 200,
+        message: "Task found successfully!",
+        data: result,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        code: 400,
+        message: e.message,
+      });
+    }
   }
 }
 
